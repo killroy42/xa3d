c15a194ece62b02fecdc175d72fc606cf17178c3
Sven Neumann <killroy@gmail.com>
2016-07-04 19:30:50 +0200

    Updated app loading

--- app.js
+++ app.js
@@ -14,10 +14,30 @@ function exec(cmd) {
 	return p;
 }
 
+function runDelayed(cmd, delay, pause) {
+	if(delay === undefined) delay = 200;
+	if(pause === undefined) pause = 200;
+	return function() {
+		return new Promise(function(resolve, reject) {
+			var logText = 'Executing: '+cmd;
+			console.time(logText);
+			setTimeout(function() {
+				exec(cmd);
+				setTimeout(function() {
+					console.timeEnd(logText);
+					resolve();
+				}, pause);
+			}, delay);
+		});
+	};
+}
 
 console.info('Launching reloaderserver and HTTP server:');
-exec('http-server.cmd ./src/www -p 80 --cors --utc --silent --proxy http://127.0.0.1:8904');
-exec('http-server.cmd ./src/www -p 8888 --cors --utc --silent --proxy http://a.tile.openstreetmap.org');
-exec('nodemon.cmd src/server/reloader/server.js -- -p 7890');
-//exec('nodemon.cmd src/server/objectserver/server.js -- -p 81');
-exec('nodemon.cmd src/server/xenonetwork/server.js -- -p 82');
+return Promise.resolve()
+	.then(runDelayed('nodemon.cmd src/server/reloader/server.js -- -p 7890', 100, 100))
+	.then(runDelayed('nodemon.cmd src/server/xenonetwork/server.js -- -p 82', 0, 200))
+	.then(runDelayed('http-server.cmd ./src/www -p 80 --cors --utc --silent'))
+	//.then(runDelayed('http-server.cmd ./src/www -p 80 --cors --utc --silent --proxy http://127.0.0.1:8904'))
+	//.then(runDelayed('http-server.cmd ./src/www -p 8888 --cors --utc --silent --proxy http://a.tile.openstreetmap.org'))
+	//exec('nodemon.cmd src/server/objectserver/server.js -- -p 81');
+	;
